8b4e79eb08ab87b26cee6272e00918cf4820e852
Curtis Jurgensen <curtisj44@hotmail.com>
2012-02-18 09:37:13 -0800

    namespaced the debug panel click event

--- static/js/debug.js
+++ static/js/debug.js
@@ -129,13 +129,13 @@
 		var closePanel = function () {
 				$panel.removeClass('open');
 				$viewButton.html($viewButton.attr('data-open'));
-				$(document).off('click');
+				$(document).off('click.debugPanel');
 			},
 			openPanel = function () {
 				$panel.addClass('open');
 				$viewButton.html($viewButton.attr('data-close'));
 
-				$(document).on('click', function (e) {
+				$(document).on('click.debugPanel', function (e) {
 					if ($(e.target).parents().filter($panel).length !== 1) {
 						closePanel();
 					}
