2a1d8180baf5c72858543929e60d8181fefe825d
Curtis Jurgensen <curtisj44@hotmail.com>
2012-07-19 22:17:53 -0700

    auto run the unit tests on page load

---
+++ static/reference/static/js/tests.js
@@ -0,0 +1,37 @@
+﻿(function (win, tests) {
+	'use strict';
+
+	tests.init = function ($tests) {
+		if (!$tests) $tests = $('a');
+
+		var i,
+			len = $tests.length,
+			$index = $('#index');
+
+		for (i = 0; i < len; i += 1) {
+			$index.append('<iframe class="tests" src="' + $tests.eq(i).attr('href') + '"></iframe>');
+		}
+	};
+
+	tests.receive = function (event, data) {
+		$('a[href="' + data.page + '"]').parent().addClass(data.result.failed > 0 ? 'failed' : 'passed');
+	};
+
+	tests.send = function () {
+		QUnit.done = function (result) {
+			win.top.$(win.top.document).trigger('done', {
+				page: $('#qunit-header > a').text().replace('js', 'htm'),
+				result: result
+			});
+		};
+	};
+
+	$(function () {
+		if (win.self === win.top) {
+			tests.init();
+			$(document).bind('done', tests.receive);
+		} else {
+			tests.send();
+		}
+	});
+}(window, window.tests = window.tests || {}));
