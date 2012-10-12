550fcac1cc65d77fcd86269be122d89af3f74782
Curtis Jurgensen <curtisj44@hotmail.com>
2012-10-11 23:29:00 -0700

    switched to simplified, standards-compliant high-res media query

--- static/js/siteName.rwd.js
+++ static/js/siteName.rwd.js
@@ -172,7 +172,7 @@
 		'M':   {'query': '(min-width:37.5em)'},		// 600px
 		'L':   {'query': '(min-width:48.0625em)'},	// 769px
 		'XL':  {'query': '(min-width:62em)'},		// 992px
-		'High-DPI': {'query': '(-moz-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (-webkit-min-device-pixel-ratio: 1.5), (min-device-pixel-ratio: 1.5), (min-resolution: 144dpi)'}
+		'High-DPI': {'query': '(-webkit-min-device-pixel-ratio:1.5), (min-resolution:144dpi), (min-resolution:1.5dppx)'}
 	};
 
 	rwd.onDelayedResize = function (callback, fireNow) {
