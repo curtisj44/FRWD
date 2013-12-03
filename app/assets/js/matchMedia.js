e4a3ff164b99553b4decf41e2b574dc8dcb20329
Curtis Jurgensen <curtisj44@hotmail.com>
2013-12-02 23:39:19 -0800

    update to latest version

--- app/assets/js/matchMedia.js
+++ app/assets/js/matchMedia.js
@@ -1,27 +1,46 @@
-/*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license */
+/*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas, David Knight. Dual MIT/BSD license */
 // https://github.com/paulirish/matchMedia.js/
-window.matchMedia = window.matchMedia || (function (doc) {
-	'use strict';
+window.matchMedia || (window.matchMedia = function() {
+	"use strict";
 
-	var bool,
-		docElem  = doc.documentElement,
-		refNode  = docElem.firstElementChild || docElem.firstChild,
-		// fakeBody required for <FF4 when executed in <head>
-		fakeBody = doc.createElement('body'),
-		div      = doc.createElement('div');
+	// For browsers that support matchMedium api such as IE 9 and webkit
+	var styleMedia = (window.styleMedia || window.media);
 
-	div.id = 'mq-test-1';
-	div.style.cssText = 'position:absolute;top:-100em';
-	fakeBody.style.background = 'none';
-	fakeBody.appendChild(div);
+	// For those that don't support matchMedium
+	if (!styleMedia) {
+		var style = document.createElement('style'),
+			script = document.getElementsByTagName('script')[0],
+			info = null;
 
-	return function (q) {
-		div.innerHTML = '&shy;<style media="' + q + '"> #mq-test-1 { width: 42px; }</style>';
+		style.type  = 'text/css';
+		style.id    = 'matchmediajs-test';
 
-		docElem.insertBefore(fakeBody, refNode);
-		bool = div.offsetWidth === 42;
-		docElem.removeChild(fakeBody);
+		script.parentNode.insertBefore(style, script);
 
-		return { matches: bool, media: q };
+		// 'style.currentStyle' is used by IE <= 8 and 'window.getComputedStyle' for all other browsers
+		info = ('getComputedStyle' in window) && window.getComputedStyle(style, null) || style.currentStyle;
+
+		styleMedia = {
+			matchMedium: function(media) {
+				var text = '@media ' + media + '{ #matchmediajs-test { width: 1px; } }';
+
+				// 'style.styleSheet' is used by IE <= 8 and 'style.textContent' for all other browsers
+				if (style.styleSheet) {
+					style.styleSheet.cssText = text;
+				} else {
+					style.textContent = text;
+				}
+
+				// Test if media query is true or false
+				return info.width === '1px';
+			}
+		};
+	}
+
+	return function(media) {
+		return {
+			matches: styleMedia.matchMedium(media || 'all'),
+			media: media || 'all'
+		};
 	};
-}(document));
+}());
